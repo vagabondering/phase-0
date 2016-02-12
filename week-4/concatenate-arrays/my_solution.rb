7d21bfa67e5c86279ad4f6564299f3853e2cdf0a
Lilian Ku <lilian.ku@gmail.com>
2016-02-12 09:39:32 -0800

    all but count-between exercises completed

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,14 @@
+# Concatenate Two Arrays
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+a = array_1
+b = array_2
+p a+b
+end
+
+array_concat(['cat', 1], [2])
---
+++ week-4/count-between/my_solution.rb
@@ -0,0 +1,42 @@
+# Count Between
+
+# I worked on this challenge [by myself, with: ].
+
+# count_between is a method with three arguments:
+#   1. An array of integers
+#   2. An integer lower bound
+#   3. An integer upper bound
+#
+# It returns the number of integers in the array between the lower and upper bounds,
+# including (potentially) those bounds.
+#
+# If +array+ is empty the method should return 0
+
+# Your Solution Below
+
+def count_between(list_of_integers, lower_bound, upper_bound)
+ if list_of_integers == []
+  return 0
+elsif upper_bound < lower_bound
+  return 0
+end
+# sum = 0
+# sum = list_of_integers[lower_bound..upper_bound].each
+# for integer in lower_bound..upper_bound
+#   p integer
+# if list_of_integers.each >= lower_bound  && list_of_integers.each <= upper_bound
+list_of_integers.each do |number| if number >= lower_bound && number <= upper_bound
+p number
+new_array = [number]
+p new_array
+sum = new_array.length
+return sum
+end
+
+end
+
+  # p list_of_integers.length
+# end
+end
+
+p count_between([1, 2, -200], 0, 100)
