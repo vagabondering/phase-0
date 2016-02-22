17ad360a7e1e2ed86ce1a463ef5493921a8fafc8
Lilian Ku <lilian.ku@gmail.com>
2016-02-22 09:23:44 -0800

    add code

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -1,21 +1,13 @@
 # Pseudocode:
 # get name inputs as an array for list.
-# Create first group with at least 3 people, go on to create another group and keep doing this until 5 groups have been created with 3 people each. Keep adding a person to each list until everyone has been filled.
+# Divide the array into sub arrays to represent each list. Print list of new arrays out.
 
 def acct_groups(names)
-  p names.length
-  group1 = []
-  group2 = []
-  group3 = []
-  group4 = []
-  group5 = []
-
-  until names.length == 0
-
-
+  if names.length > 5
+    group = []
+    group = names.each_slice(5).to_a
+    p group
+  end
 end
 
 acct_groups(["John","Lisa","Annie","James", "Peter", "Daniel", "Tom", "Paul", "Lara", "Emily", "Andrew", "Tim", "Ray", "Pat", "Jenna", "Kim", "Ben", "Rita", "Wes", "Archie", "Don", "Calvin", "Jason"])
-
-
-# p array_of_students = (1..57).to_a
