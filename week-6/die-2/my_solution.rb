0591dc621f4e268e8ad168d0b5f23f2c5382e348
Lilian Ku <lilian.ku@gmail.com>
2016-02-22 22:49:27 -0800

    Add reflections

--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -2,7 +2,7 @@
 
 
 # I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I spent [.5] hours on this challenge.
 
 # Pseudocode
 
@@ -47,3 +47,15 @@ p my_die.roll
 
 
 # Reflection
+
+# What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+# The main difference was the input and working with array of strings instead of an integer. The logic was slightly different since the output was different as well so instead of using rand method, sample method was used for roll method, as well as .length for the side method.
+
+# What does this exercise teach you about making code that is easily changeable or modifiable?
+# It taught me how easy it is to take an existing class and changing it a bit to produce a different object.
+
+# What new methods did you learn when working on this challenge, if any?
+# Using the method sample was new to me, I now learned to how to randomly choose a string from an array.
+
+# What concepts about classes were you able to solidify in this challenge?
+# I felt I learned to be more comfortable creating a class and its structure and methods and executing those methods.
--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -2,7 +2,7 @@
 
 
 # I worked on this challenge [by myself, with: ].
-# I spent [#] hours on this challenge.
+# I spent [1] hours on this challenge.
 
 # Pseudocode
 
@@ -14,14 +14,13 @@
 # if guess is == answer, return :correct,
 # if guess < answer, return :low
 # for solved method:
-# find a way to get latest correct answer, return true otherwise false
+# Test to see if the guess is solved, return true if guess matches answer, otherwise false
 
 # Initial Solution
 
 class GuessingGame
 
   def initialize(answer)
-    # @guess = guess
     @answer = answer
   end
 
@@ -65,12 +64,14 @@ game.solved?
 # Reflection
 
 # How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
-
+# I'm not sure I understand this question.
 
 # When should you use instance variables? What do they do for you?
-
+# I use an instance variable when I want to be able to use the same variables for more than one method.
 
 # Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
-
+# I use flow control to be able to get different scenarios depending on the condition. I would start with first scenario and if that passes the first condition, the first scenario returns. If the first condition isn't met, then it goes on to the next one and repeats the same action as the first scenario. It keeps going depending on how many conditional statements has been set.
 
 # Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+# The benefits of using symbols is for the esthetic and faster use. Compared to using strings, using symbols is faster because Ruby doesn't have to do the step of turning the string into symbols.
+# As for this exercise, I'm not sure why a symbol had to be used here but my guess is because a symbol remains as the same object by having same object_id each time it's executed, whereas a string's object_id changes everytime it's executed and I'm assuming that's needed for the solve method.
