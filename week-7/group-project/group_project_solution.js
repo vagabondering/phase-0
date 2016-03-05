81e8a7aa1d32729b0da9a39094b682f64f002e5b
Lilian Ku <lilian.ku@gmail.com>
2016-03-05 11:43:01 -0800

    Add release 2 for 7.8 exercise

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,18 @@
+//User Story by kevin
+
+// As a user, I want to be able to input any list of numbers and find the following values for the items in the list: the SUM, the MEAN and the MEDIAN.  The input can be a list of numbers with an odd or even length, but if the length of the list is even, the median needs to return a float value of the middle of the list (for example, a list containting 1, 2, 3, and 4 would return 2.5 as a median.)
+
+ // User Stories to Pseudocode by Li-Lian
+
+ // Create object that takes an input of array.
+
+ // Create function for SUM that takes the array object as an argument. Use for loop to iterate through each array element:
+ // For var num equal zero, stop when less than array length and then increments by 1. Add numbers up to get SUM.
+
+ // For MEAN function, divide total of SUM by length of input array object to calculate MEAN.
+
+ // For MEDIAN function,
+ // If the length of the array object is even, find the two middle elements, sum them up and and divide it by two.
+ // else
+  // Find middle element of the array and divide it by two.
+
